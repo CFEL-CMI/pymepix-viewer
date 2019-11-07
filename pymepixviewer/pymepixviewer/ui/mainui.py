18de8ed62587432e97c065b047b80443f24f50a3
brombh <hubertus.bromberger@cfel.de>
2019-11-07 15:07:02 +0100

    add shortcut to menu bar and pass arguments to updsampler parent class

--- pymepixviewer/pymepixviewer/ui/mainui.py
+++ pymepixviewer/pymepixviewer/ui/mainui.py
@@ -16,7 +16,7 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
@@ -43,9 +43,19 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuFile.setTitle(_translate("MainWindow", "&File"))
         self.menuLoad_Setting.setTitle(_translate("MainWindow", "Load Setting"))
         self.actionSophy_spx.setText(_translate("MainWindow", "SoPhy file"))
         self.actionLoad_Hotpixel_Mask.setText(_translate("MainWindow", "Load Hotpixel Mask"))
         self.actionHot_Pixel_Mask.setText(_translate("MainWindow", "Hot Pixel Mask"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
