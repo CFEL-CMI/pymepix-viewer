f7d95ece3156dc2bdd8cc32d212da4f641b4a6b7
BenMoon <brombh@desy.de>
2021-06-08 11:34:36 +0200

    Fix non-existing returning variable

--- pymepixviewer/panels/regionsofinterest.py
+++ pymepixviewer/panels/regionsofinterest.py
@@ -224,7 +224,6 @@ class RoiModel(QtCore.QAbstractItemModel):
         self._old_roi.roiUpdated.disconnect(self.onRoiUpdate)
 
         # rint(self.rootItem)
-        return roi
 
     def index(self, row, column, parent):
 
