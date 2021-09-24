#13 Adjust the calculation of the features mean, RMS, ...) - previous calculation was wrong (calculated properties of the histogram not of the histograms data)

pymepixviewer/panels/timepix_setup_histogram.py
@@ -1,4 +1,5 @@
 from collections import deque
+import math
 
 import pyqtgraph as pg
 import numpy as np
@@ -6,7 +7,7 @@ from scipy.optimize import curve_fit
 
 class TimepixSetupHistogram():
 
-    def __init__(self, plt, label_left, label_bottom, units):
+    def __init__(self, plt, label_left, label_bottom, units=None):
         self.plt = plt
         self.plot_data_item = self.__setup_hist_ui(label_left, label_bottom, units)
         self.snapshot_plot_data_item = self.__add_plot_data_item(fillLevel=0, brush=(255, 0, 0, 75))
@@ -72,7 +73,7 @@ class TimepixSetupHistogram():
         y, x = np.histogram(tot, tot_bins)
         self.buffer.append(y)
         y = sum(self.buffer)
-        self.plot_data_item.setData(x, y, stepMode='center')
+        self.plot_data_item.setData(x, y , stepMode='center')
 
         index = y.argmax()
         y_max = y.max()
@@ -86,12 +87,29 @@ class TimepixSetupHistogram():
         A, mu, fwhm = popt
         self.legend.getLabel(self.plot_data_item).setText(f'Data ({len(self.buffer)} packets)')
         self.legend_label_fwhm.setText(f'FWHM: {fwhm:.2f}')
-        self.legend_label_mean.setText(f'mean: {np.mean(y) / len(self.buffer):.2f}')
-        self.legend_label_standard_deviation.setText(f'std.: {np.std(y) / len(self.buffer):.2f}')
-        self.legend_label_rms.setText(f'RMS: {np.sqrt(np.mean(np.square(y))) / len(self.buffer):.2f}')
+        self.legend_label_mean.setText(f'mean: {TimepixSetupHistogram.approx_mean(x[:-1], y):.2f}')
+        self.legend_label_standard_deviation.setText(f'std.: {TimepixSetupHistogram.approx_std(x[:-1], y):.2f}')
+        self.legend_label_rms.setText(f'RMS: {TimepixSetupHistogram.approx_rms(x[:-1], y):.2f}')
         self.plot_curve_item.setData(x, TimepixSetupHistogram.__gauss_fwhm(x, *popt))
 
     @staticmethod
     def __gauss_fwhm(x, *p):
         A, mu, fwhm = p
         return A * np.exp(-((x - mu) ** 2) / (2.0 * (fwhm ** 2) / (4 * 2 * np.log(2))))
+
+    @staticmethod
+    def approx_mean(x, y):
+        return (x * y).sum() / y.sum()
+
+    @staticmethod
+    def approx_std(x, y):
+        """ Calculation of the approximated standard deviation of the histogram according to: https://math.stackexchange.com/a/857576"""
+        mean = TimepixSetupHistogram.approx_mean(x, y)
+        size = y.sum()
+
+        return math.sqrt((((x - mean)**2 * y) / size).sum())
+    
+    @staticmethod
+    def approx_rms(x, y):
+        """ Calculation of the approximated RMS of the histogram according to: https://de.mathworks.com/matlabcentral/answers/377179-root-mean-square-value-of-histogram"""
+        return np.sqrt(np.sum(np.square(x) * y) / y.sum())

pymepixviewer/panels/timepixsetupplotspanel.py
@@ -53,13 +53,13 @@ class TimepixSetupPlotsPanel(QtGui.QDockWidget, Ui_DockWidget):
         result = super().setupUi(dock_widget)
 
         # Event Data Plots Preparation
-        self._event_data_tot_histogram = TimepixSetupHistogram(self.plt_event_data_histogram_tot, 'Count', 'ToT', 'ns')
+        self._event_data_tot_histogram = TimepixSetupHistogram(self.plt_event_data_histogram_tot, 'Count', 'ToT (ns)')
         self.plt_event_data_2d_histogram_tof_tot = self.__setup_2d_hist_ui("plt_event_data_2d_histogram_tof_tot", (2, 0), 'ToT', 'ToF')
 
         # Centroided Data Plots Preparation
-        self._centroided_data_mean_tot_histogram = TimepixSetupHistogram(self.plt_centroided_data_histogram_mean_tot, 'Count', 'Mean ToT', 'ns')
+        self._centroided_data_mean_tot_histogram = TimepixSetupHistogram(self.plt_centroided_data_histogram_mean_tot, 'Count', 'Mean ToT (ns)')
         
-        self._centroided_data_max_tot_histogram = TimepixSetupHistogram(self.plt_centroided_data_histogram_max_tot, 'Count', 'Max ToT', 'ns')
+        self._centroided_data_max_tot_histogram = TimepixSetupHistogram(self.plt_centroided_data_histogram_max_tot, 'Count', 'Max ToT (ns)')
 
         self.plt_centroided_data_2d_histogram_tof_mean_tot = self.__setup_2d_hist_ui("plt_centroided_data_2d_histogram_tof_mean_tot", (2, 1), 'Mean ToT', 'ToF')
 
@@ -167,18 +167,18 @@ class TimepixSetupPlotsPanel(QtGui.QDockWidget, Ui_DockWidget):
         self.__tof_bins = np.linspace(tof_min, tof_max, 50)
 
     def __update_events(self, tof, tot):
-        self._event_data_tot_histogram.refresh(tot, self.__tot_bins)
+        self._event_data_tot_histogram.refresh(tot, np.array(self.__tot_bins))
         self.__plot_2d_histogram(tot, tof, (self.__tot_bins, self.__tof_bins), self.__events_2d_hist_buffer, self.plt_event_data_2d_histogram_tof_tot)
 
     def __update_centroids(self, tof, tot_mean, tot_max, cluster_size):
         # ToT (mean) histogram
-        self._centroided_data_mean_tot_histogram.refresh(tot_mean, self.__tot_bins)
+        self._centroided_data_mean_tot_histogram.refresh(tot_mean, np.array(self.__tot_bins))
 
         # ToF-ToT (mean) correlation
         self.__plot_2d_histogram(tot_mean, tof, (self.__tot_bins, self.__tof_bins), self.__centroids_2d_hist_mean_buffer, self.plt_centroided_data_2d_histogram_tof_mean_tot)
 
         # ToT (max) histogram
-        self._centroided_data_max_tot_histogram.refresh(tot_max, self.__tot_bins)
+        self._centroided_data_max_tot_histogram.refresh(tot_max, np.array(self.__tot_bins))
 
         # ToF-ToT (max) correlation
         self.__plot_2d_histogram(tot_max, tof, (self.__tot_bins, self.__tof_bins), self.__centroids_2d_hist_max_buffer, self.plt_centroided_data_2d_histogram_tof_max_tot)
@@ -196,7 +196,7 @@ class TimepixSetupPlotsPanel(QtGui.QDockWidget, Ui_DockWidget):
         y0, y1 = (bins[1][0], bins[1][-1])
         xscale = (x1-x0) / img.shape[0]
         yscale = (y1-y0) / img.shape[1]
-        plt.setImage(img / img.max(), scale=[xscale, yscale], pos=[x0, y0], autoRange=False, autoLevels=False, autoHistogramRange=False)
+        plt.setImage(img / img.max(), scale=[xscale, yscale / 1000], pos=[x0, y0 / 1000], autoRange=False, autoLevels=False, autoHistogramRange=False)
 
     def on_event(self, events):
         self.__update_events(events[3], events[4])